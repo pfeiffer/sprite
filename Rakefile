4d22ce0d5676eaccd78e481af026343915edbdd3
Mattias Pfeiffer <mattias@pfeiffer.dk>
2010-11-02 09:38:08 +0700

    Updating

--- Rakefile
+++ Rakefile
@@ -1,22 +1,21 @@
 require 'rake'
-require 'spec/rake/spectask'
+#require 'spec/rake/spectask'
 require 'jeweler'
 
-desc 'Default: run unit tests.'
-task :default => :spec
+#desc 'Default: run unit tests.'
+#task :default => :spec
 
-desc "Run all specs in spec directory"
-Spec::Rake::SpecTask.new(:spec) do |t|
-  t.spec_files = FileList['spec/**/*_spec.rb']
-end
+#desc "Run all specs in spec directory"
+#Spec::Rake::SpecTask.new(:spec) do |t|
+#  t.spec_files = FileList['spec/**/*_spec.rb']
+#end
 
 Jeweler::Tasks.new do |gemspec|
-  gemspec.name = "sprite"
+  gemspec.name = "mpf-sprite"
   gemspec.summary = "generate your css sprites automagically"
   gemspec.description = "sprite is a rails/merb plugin that generates sprites for css, sass"
-  gemspec.email = "merbjedi@gmail.com"
-  gemspec.homepage = "http://github.com/merbjedi/sprite" 
-  gemspec.authors = ["Jacques Crocker"]
+  gemspec.email = "mattias@pfeiffer.dk"
+  gemspec.authors = ["Jacques Crocker", "mpf"]
   gemspec.files.exclude '.gitignore'
   
   # removing test files and specs from the gem to save space
@@ -24,4 +23,3 @@ Jeweler::Tasks.new do |gemspec|
   gemspec.test_files = []
 end
 Jeweler::GemcutterTasks.new
-
--- lib/sprite/styles/sass_mixin_new_generator.rb
+++ lib/sprite/styles/sass_mixin_new_generator.rb
@@ -11,7 +11,7 @@ module Sprite
         File.open(File.join(Sprite.root, path), 'w') do |f|
           add_else = false
 
-          f.puts "@mixin sprite($group_name, $image_name, $offset=0, $include_dimensions=true) {"
+          f.puts "@mixin sprite($group_name, $image_name, $offset=0, $include_dimensions=true, $override_offset = false) {"
           sprite_files.each do |sprite_file, sprites|
             sprites.each do |sprite|
               f << "  @"
@@ -22,9 +22,11 @@ module Sprite
               #{sprite[:x]}px #{sprite[:y]}px
               
               if sprite[:align] == 'horizontal'
-                background_offset = "\#{#{sprite[:x]}+$offset}px #{sprite[:y]}px"
+                background_offset           = "\#{#{sprite[:x]}+$offset}px #{sprite[:y]}px"
+                background_offset_override  = "\#{#{sprite[:x]}+$offset}px $override_offset"
               else
-                background_offset = "#{sprite[:x]}px \#{#{sprite[:y]}+$offset}px"
+                background_offset           = "#{sprite[:x]}px \#{#{sprite[:y]}+$offset}px"
+                background_offset_override  = "$override_offset \#{#{sprite[:y]}+$offset}px"
               end
               
               f.puts "if $group_name == \"#{sprite[:group]}\" and $image_name == \"#{sprite[:name]}\" {"
@@ -32,6 +34,9 @@ module Sprite
               f.puts "    @if($include_dimensions) {"
               f.puts "      width: #{sprite[:width]}px;"
               f.puts "      height: #{sprite[:height]}px;"
+              f.puts "    }"
+              f.puts "    @if($override_offset) {"
+              f.puts "      background-position: #{background_offset_override};"
               f.puts "    }"
               f.puts "  }"
             end
